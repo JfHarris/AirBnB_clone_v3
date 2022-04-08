1c7a785d0b373e2b9f24f68eb9904a33cc6dcbdc
Code Slayer <3613@holbertonschool.com>
2022-04-08 14:38:32 -0500

    start of tasks 1-3

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    myDict = {}
+    $( 'li input[type="checkbox"]' ).on( "click", function() {
+        myDict[($( this ).data('id'))] = $( this ).data('name');
+      });
+    $( 'li input[type="checkbox"]' ).off( "click", function() {
+        delete myDict[$( this ).data('id')];
+      });
+    $( ".amenities h4" ).text(myDict)
+  });
