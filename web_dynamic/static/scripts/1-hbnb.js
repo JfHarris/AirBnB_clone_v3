247d36a364588d60dd89341cb35a44df2503d146
Code Slayer <3613@holbertonschool.com>
2022-04-12 13:14:29 -0500

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    myDict = {}
-    $( 'input' ).change( function() {
-      if ($(this).is(":checked")) {
-        myDict[($( this ).data('id'))] = ' ' + $( this ).data('name');
-      }else{
-        delete myDict[$( this ).data('id')];
-      }
-      $( ".amenities h4" ).text(Object.values(myDict))
-    });
+  myDict = {}
+  $( 'input' ).change( function() {
+    if ($(this).is(":checked")) {
+      myDict[($( this ).data('id'))] = ' ' + $( this ).data('name');
+    }else{
+      delete myDict[$( this ).data('id')];
+    }
+    $( ".amenities h4" ).text(Object.values(myDict))
+    if ($(this).width() > $(".amenities").width()){
+      $( ".amenities h4" ).css("color", "red")
+
+    }
   });
+});
